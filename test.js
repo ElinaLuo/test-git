35245b2355c96bb27165b4654582e85a48f95402
luorongrong <luorongrong@meituan.com>
2019-06-25 10:48:55 +0800

    完整版

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
 console.log('object');
-<<<<<<< HEAD
 console.log('commit 1');
 console.log('commit 2');
 console.log('commit 3');
-=======
-console.log('commit 1');
->>>>>>> parent of 297dad4... commit 2
