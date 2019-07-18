8e83b59e3c4b585627e5149cc359d1310e78389b
luorongrong <luorongrong@meituan.com>
2019-07-18 16:14:24 +0800

    master branch

--- test.js
+++ test.js
@@ -1,6 +1,1 @@
-console.log('object');
-console.log('commit 1');
-console.log('commit 2');
-console.log('commit 3');
-console.log('pull msg for b2');
-console.log('pull msg for b1');
+console.log('this is test file');
